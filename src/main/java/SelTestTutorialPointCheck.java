3d582aec3d6a3f3de880d54ce85b2f89a66a1d35
Gringo Gang <miroslav.pecek1@gmail.com>
2025-08-12 17:13:53 +0200

    commit

--- src/main/java/SelTestTutorialPointCheck.java
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -1,5 +1,14 @@
+import org.jspecify.annotations.Nullable;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+import java.util.Set;
 
 
 public class SelTestTutorialPointCheck {
@@ -9,10 +18,21 @@ public class SelTestTutorialPointCheck {
     @BeforeEach
     public void setUp(){
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\MiroslavPěček\\IdeaProjects\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
+        ovladac = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown(){
+        ovladac.quit();
+    }
+
+    @Test
+    public void checkboxTest() {
+
+        ovladac.get("https://www.tutorialspoint.com/selenium/practice/check-box.php");
 
 
 
     }
-
 
 }
