9014a25d9a0e3c26a7b54730048722d08abbc069
Gringo Gang <miroslav.pecek1@gmail.com>
2025-08-12 17:06:43 +0200

    init commit

---
+++ src/main/java/SelTestTutorialPointCheck.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+
+
+public class SelTestTutorialPointCheck {
+
+    private WebDriver ovladac;
+
+    @BeforeEach
+    public void setUp(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MiroslavPěček\\IdeaProjects\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
+
+
+
+    }
+
+
+}
