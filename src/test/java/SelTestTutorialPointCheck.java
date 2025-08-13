7bca2019608f0de2521c2ce7e8ef568831dca1e4
Gringo Gang <miroslav.pecek1@gmail.com>
2025-08-13 16:08:17 +0200

    commit chromDriver

--- src/test/java/SelTestTutorialPointCheck.java
+++ src/test/java/SelTestTutorialPointCheck.java
@@ -1,29 +1,26 @@
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.jspecify.annotations.Nullable;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
+import java.util.List;
+import java.util.Set;
 
 
 public class SelTestTutorialPointCheck {
 
     private WebDriver ovladac;
 
-
     @BeforeEach
     public void setUp(){
-        //System.setProperty("webdriver.chrome.driver", "C:\\Users\\MiroslavPěček\\IdeaProjects\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
-        //ovladac = new ChromeDriver();
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\MiroslavPěček\\IdeaProjects\\SeleniumSkillmea\\src\\main\\resources\\chromedriver.exe");
         ovladac = new ChromeDriver();
-
     }
-
 
     @AfterEach
     public void tearDown(){
